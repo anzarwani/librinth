9e0d85e6a0978628994c424ab82be01c7c7b2868
anzarhw <71497005+anzarhw@users.noreply.github.com>
2022-09-27 19:33:33 +0530

    deploy to railway

--- ablog/settings.py
+++ ablog/settings.py
@@ -28,7 +28,7 @@ with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
